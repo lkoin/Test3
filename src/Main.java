4833f872c99f124784b89dd6bb10965562c5db4f
lkoin <lkoin.kontakt@gmail.com>
2023-09-08 09:18:39 +0200

    Factorial calculator

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
+        int liczba;
+        System.out.println("Podaj liczbe: ");
+        liczba = scan.nextInt();
+        int liczba2 = 1;
+        for (int i = 1; i <= liczba; i++) {
+            liczba2 = liczba2*i;
 
 
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
         }
+        System.out.println("Silnia z liczby "+ liczba + " 3wynosi " + liczba2);
     }
 }
